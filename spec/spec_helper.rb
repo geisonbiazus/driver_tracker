cb6054cf8b30c4db80c8c34c35066ca78f4d58b2
Geison Biazus <geisonbiazus@gmail.com>
2018-03-30 15:54:40 -0300

    Add Geokit gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require 'date'
 require 'ostruct'
 require 'time'
+require 'geokit'
 
 require File.join(__dir__, '..', 'lib', 'driver_activity', 'company')
 require File.join(__dir__, '..', 'lib', 'driver_activity', 'event')
