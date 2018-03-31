b09898e4f0106914ddb1cffe54f4958dae40dc2b
Geison Biazus <geisonbiazus@gmail.com>
2018-03-30 22:11:50 -0300

    Add CompanyInteractor

---
+++ lib/driver_activity/company_interactor.rb
@@ -0,0 +1,48 @@
+module DriverActivity
+  class CompanyInteractor
+    INVALID = 'INVALID'.freeze
+
+    def initialize(company_repository)
+      @company_repository = company_repository
+    end
+
+    def create(field:)
+      errors = validate_polygon(field)
+
+      return error_response(errors) unless errors.empty?
+
+      company = Company.new(field: field)
+      success_response(@company_repository.create(company))
+    end
+
+    private
+
+    def validate_polygon(field)
+      errors = []
+      unless valid_polygon?(field)
+        errors << { field: :field, type: INVALID }
+      end
+      errors
+    end
+
+    def valid_polygon?(polygon)
+      return false unless polygon.is_a?(Array)
+      polygon.each do |point|
+        return false unless point.is_a?(Array)
+        return false unless point.size == 2
+        point.each do |axis|
+          return false unless axis.is_a?(Numeric)
+        end
+      end
+      true
+    end
+
+    def error_response(errors)
+      { status: :error, errors: errors }
+    end
+
+    def success_response(company)
+      { status: :success, company: company }
+    end
+  end
+end
---
+++ spec/lib/driver_activity/company_interactor_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+module DriverActivity
+  RSpec.describe CompanyInteractor do
+    let(:created_company) { Company.new(id: 1) }
+    let(:company_repository) { spy(:company_repository, create: created_company) }
+    let(:interactor) { described_class.new(company_repository) }
+
+    describe 'create' do
+      context 'with valid field polygon' do
+        let(:field) { [[0.5, 0.5], [0, 5], [5, 5]] }
+        let(:company) { Company.new(field: field) }
+        let(:created_company) { Company.new(id: 1, field: field) }
+        let(:result) { interactor.create(field: field) }
+
+        it 'creates a new company' do
+          result
+          expect(company_repository).to have_received(:create).with(company)
+        end
+
+        it 'returns success and the created company' do
+          expect(result[:status]).to eq :success
+          expect(result[:company]).to eq created_company
+        end
+      end
+    end
+
+    context 'with invalid field polygon' do
+      let(:invalid_polygons) do
+        [
+          nil,
+          '',
+          'asdas',
+          8,
+          [1],
+          [[1, 2, 3]],
+          [[1, 'asd']]
+        ]
+      end
+
+      it 'returns error' do
+        invalid_polygons.each do |polygon|
+          result = interactor.create(field: polygon)
+          expect(result[:status]).to eq :error
+          expect(result[:errors]).to eq(
+            [{ field: :field, type: described_class::INVALID }]
+          )
+        end
+      end
+    end
+  end
+end
