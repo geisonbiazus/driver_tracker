9d3955f29a99abb92938cc87d09b7b12bdf18221
Geison Biazus <geisonbiazus@gmail.com>
2018-03-31 10:54:37 -0300

    Add some shortcuts to access and configure resources

--- lib/driver_tracker.rb
+++ lib/driver_tracker.rb
@@ -14,4 +14,23 @@ require_relative 'driver_tracker/activity/generate_report_interactor'
 require_relative 'driver_tracker/activity/add_event_interactor'
 
 module DriverTracker
+  class << self
+    attr_accessor :company_repository
+    attr_accessor :event_repository
+
+    def company_interactor
+      CompanyInteractor.new(company_repository)
+    end
+
+    def add_event_interactor
+      Activity::AddEventInteractor.new(company_repository, event_repository)
+    end
+
+    def generate_report_interactor
+      Activity::GenerateReportInteractor.new(event_repository)
+    end
+  end
+
+  self.company_repository = Repositories::CompanyRepository.new
+  self.event_repository = Repositories::EventRepository.new
 end
