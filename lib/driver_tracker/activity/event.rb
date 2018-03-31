b41c270151fc0e1e8fe69282d671f1bd6bc4b8d6
Geison Biazus <geisonbiazus@gmail.com>
2018-03-31 12:51:24 -0300

    Transform openstructs into normal objects
    It is best for attributes safety and to know what attributes exist in
    the entity

--- lib/driver_tracker/activity/event.rb
+++ lib/driver_tracker/activity/event.rb
@@ -1,10 +1,42 @@
 module DriverTracker
   module Activity
-    class Event < OpenStruct
+    class Event
       CULTIVATING = 'cultivating'.freeze
       REPAIRING = 'repairing'.freeze
       DRIVING = 'driving'.freeze
       STOPPED = 'stopped'.freeze
+
+      attr_accessor :company_id, :driver_id, :timestamp, :latitude,
+                    :longitude, :accuracy, :speed, :activity
+
+      def initialize(company_id: nil,
+                     driver_id: nil,
+                     timestamp: nil,
+                     latitude: nil,
+                     longitude: nil,
+                     accuracy: nil,
+                     speed: nil,
+                     activity: nil)
+        self.company_id = company_id
+        self.driver_id = driver_id
+        self.timestamp = timestamp
+        self.latitude = latitude
+        self.longitude = longitude
+        self.accuracy = accuracy
+        self.speed = speed
+        self.activity = activity
+      end
+
+      def ==(other)
+        company_id == other.company_id &&
+          driver_id == other.driver_id &&
+          timestamp == other.timestamp &&
+          latitude == other.latitude &&
+          longitude == other.longitude &&
+          accuracy == other.accuracy &&
+          speed == other.speed &&
+          activity == other.activity
+      end
     end
   end
 end
--- lib/driver_tracker/company.rb
+++ lib/driver_tracker/company.rb
@@ -1,4 +1,14 @@
 module DriverTracker
-  class Company < OpenStruct
+  class Company
+    attr_accessor :id, :field
+
+    def initialize(id: nil, field: nil)
+      self.id = id
+      self.field = field
+    end
+
+    def ==(other)
+      id == other.id && field == other.field
+    end
   end
 end
