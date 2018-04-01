e235f56155c02cd74d83d3ec15682e64dc2a057e
Geison Biazus <geisonbiazus@gmail.com>
2018-04-01 11:50:05 -0300

    Validate report parameters

--- app/controllers/activity_report_controller.rb
+++ app/controllers/activity_report_controller.rb
@@ -1,6 +1,9 @@
 class ActivityReportController < ApplicationController
   def index
+    return render status: :bad_request if driver_id.zero? || !date
+
     @report = DriverTracker.generate_report_interactor.run(driver_id, date)
+    render status: :ok
   end
 
   private
@@ -10,7 +13,9 @@ class ActivityReportController < ApplicationController
   end
 
   def date
-    report_params[:date].to_date
+    Date.parse(report_params[:date])
+  rescue TypeError, ArgumentError
+    nil
   end
 
   def report_params
--- spec/controllers/activity_report_controller_spec.rb
+++ spec/controllers/activity_report_controller_spec.rb
@@ -2,18 +2,34 @@ require 'rails_helper'
 
 RSpec.describe ActivityReportController, type: :controller do
   describe 'GET #index' do
-    let(:company) { create(:company) }
-    let(:events) do
-      [
-        create(:activity_event, company: company, driver_id: 1),
-        create(:activity_event, company: company, driver_id: 1),
-        create(:activity_event, company: company, driver_id: 1)
-      ]
+    context 'with valid params' do
+      let(:company) { create(:company) }
+      let(:events) do
+        [
+          create(:activity_event, company: company, driver_id: 1),
+          create(:activity_event, company: company, driver_id: 1),
+          create(:activity_event, company: company, driver_id: 1)
+        ]
+      end
+
+      it 'generates the report' do
+        get :index, params: { driver_id: 1, date: Date.today }
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'generates the report' do
-      get :index, params: { driver_id: 1, date: Date.today }
-      expect(response).to have_http_status(:success)
+    context 'with invalid params' do
+      it 'generates the report' do
+        get :index, params: { }
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context 'with invalid date' do
+      it 'generates the report' do
+        get :index, params: { driver_id: 1, date: '2018-04-0' }
+        expect(response).to have_http_status(:bad_request)
+      end
     end
   end
 end
